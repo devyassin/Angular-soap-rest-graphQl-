88c305428d2117f7d419eb7707a21ef1d5547c9a
yassine lamouadden <96653739+devyassin@users.noreply.github.com>
2025-01-29 21:14:18 +0100

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   API_URL: 'https://pokeapi.co/api/v2/pokemon',
   API_SOAP: 'http://localhost:8090',
   API_GraphQL: 'http://localhost:8060/graphql',
-  API_OCR: 'http://35.180.128.207/api/v1',
+  API_OCR: 'https://ec2-35-180-128-207.eu-west-3.compute.amazonaws.com/api/v1',
 };
